ae30f43b736b610b4313710d370263e4c4eb9322
lisk-patrick <patrick.lisk314@gmail.com>
2018-04-29 17:27:17 -0500

    total months

---
+++ PyBank/main.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+budgetReader = []
+budgetData = []
+csvpath1 = os.path.join("..","HOMEWORK","Instructions","PyBank","raw_data", "budget_data_1.csv")
+
+with open(csvpath1, newline="") as csvfile:
+    budgetReader = csv.reader(csvfile, delimiter= ",")
+    
+    total_revenue = 0
+    for row in budgetReader:
+        budgetHeader = [row[0]]
+        row_count = sum(1 for row in budgetReader)
+        total_months = int(row_count - 1)
+
+        for i in range(2, row_count):
+            total_revenue += float(i[1])
+    print(total_months)
+    print(total_revenue)
