fa805b9b16e30a4d54c88446b105ddf273a8e91a
lisk-patrick <patrick.lisk314@gmail.com>
2018-04-29 20:38:19 -0500

    monthly average diff. pt1

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,32 +2,51 @@ import csv
 import os
 
 budgetReader1 = []
+date = []
+revenue = []
 budgetData = []
 csvpath1 = os.path.join("..","HOMEWORK","Instructions","PyBank","raw_data", "budget_data_1.csv")
-csvpath2 = os.path.join("..", "HOMEWORK", "Instructions", "PyBank", "raw_data", "budget_data_2.csv")
+#csvpath2 = os.path.join("..", "HOMEWORK", "Instructions", "PyBank", "raw_data", "budget_data_2.csv")
 
 with open(csvpath1, newline="") as csvfile1:
     budgetReader1 = csv.reader(csvfile1, delimiter= ",")
     
-    total_revenue = 0
+    current_revenue = 0
+    sum_month2month_revenue = 0
+   
     for row in budgetReader1:
-        budgetHeader = [row[0]]
-        row_count = sum(1 for row in budgetReader1)
-        total_months = int(row_count)
-
+        month_count = sum(1 for row in budgetReader1)
+        total_months = int(month_count)
+        date.append(row[0])
+        revenue.append(row[1])
+        budgetData = zip(date,revenue)
+        
+        current_revenue = row[1]
+        next_revenue = next(row[1])
+        if next_revenue == 0:
+            break
+        else:
+            month2month_difference = next_revenue - current_revenue
+            sum_month2month_revenue += float(current_revenue)
+            month2month_avg = float(sum_month2month_revenue / 2)
+            sum_month2month_revenue = 0
+        
        # for i in range(2, row_count):
+    
            # total_revenue += float(i[1])
     print(total_months)
-    print(total_revenue)
+    print(month2month_avg)
+    print(budgetData)
 
-    with open(csvpath2, newline="") as csvfile2:
-        budgetReader2 = csv.reader(csvfile2, delimiter=",")
+    #with open(csvpath2, newline="") as csvfile2:
+     #   budgetReader2 = csv.reader(csvfile2, delimiter=",")
 
-        total_revenue = 0
-        for row in budgetReader2:
-            row_count = sum(1 for row in budgetReader2)
-            total_months = int(row_count)
+      #  total_revenue = 0
+       # for row in budgetReader2:
+        #    row_count = sum(1 for row in budgetReader2)
+         #   total_months = int(row_count)
             #for i in range(2,row_count):
                 #total_revenue += float(i[1])
-        print(total_months)
-        print(total_revenue)
+     #print(total_months)
+        #print(total_revenue)
+    
